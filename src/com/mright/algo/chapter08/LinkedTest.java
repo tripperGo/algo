665cfac41dbfc00b9d846a3d090fd42209f316ca
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2020-07-23 11:16:10 +0800

    动态代理

---
+++ src/com/mright/algo/chapter08/LinkedTest.java
@@ -0,0 +1,10 @@
+package com.mright.algo.chapter08;
+
+public class LinkedTest {
+
+    public static void main(String[] args) {
+        int i = 4;
+        int j = 6;
+        System.out.println(j + i);
+    }
+}
